7524baea60bc2ee7f288290ac584d03091a9bad0
Kulcsar Rudolf <kulcsarrudolf@gmail.com>
2023-11-04 22:40:26 +0200

    Gifts

---
+++ src/data/gifts.js
@@ -0,0 +1,154 @@
+const gifts = [
+  {
+    name: "szervezés",
+    partA: [1, 61, 121],
+    partB: [31, 91, 151],
+  },
+  {
+    name: "misszionárius",
+    partA: [2, 62, 122],
+    partB: [32, 92, 152],
+  },
+  {
+    name: "házasság nélküli élet",
+    partA: [3, 63, 123],
+    partB: [33, 93, 153],
+  },
+  {
+    name: "lelkek megkülönböztetése",
+    partA: [4, 64, 124],
+    partB: [34, 94, 154],
+  },
+  {
+    name: "evangélizálás",
+    partA: [5, 65, 125],
+    partB: [35, 95, 155],
+  },
+  {
+    name: "lelkigondozás",
+    partA: [6, 66, 126],
+    partB: [36, 96, 156],
+  },
+  {
+    name: "hit",
+    partA: [7, 67, 127],
+    partB: [37, 97, 157],
+  },
+  {
+    name: "adás-adakozás",
+    partA: [8, 68, 128],
+    partB: [38, 98, 158],
+  },
+  {
+    name: "ördögűzés",
+    partA: [9, 69, 129],
+    partB: [39, 99, 159],
+  },
+  {
+    name: "gyógyítás",
+    partA: [10, 70, 130],
+    partB: [40, 100, 160],
+  },
+  {
+    name: "tanítás",
+    partA: [11, 71, 131],
+    partB: [41, 101, 161],
+  },
+  {
+    name: "nyelvekenszólás",
+    partA: [12, 72, 132],
+    partB: [42, 102, 162],
+  },
+  {
+    name: "bölcsesség beszéde",
+    partA: [13, 73, 133],
+    partB: [43, 103, 163],
+  },
+  {
+    name: "önkéntes szegénység",
+    partA: [14, 74, 134],
+    partB: [44, 104, 164],
+  },
+  {
+    name: "szakmunka",
+    partA: [15, 75, 135],
+    partB: [45, 105, 165],
+  },
+  {
+    name: "segítés",
+    partA: [16, 76, 136],
+    partB: [46, 106, 166],
+  },
+  {
+    name: "vendéglátás",
+    partA: [17, 77, 137],
+    partB: [47, 107, 167],
+  },
+  {
+    name: "imádság",
+    partA: [18, 78, 138],
+    partB: [48, 108, 168],
+  },
+  {
+    name: "nyelvek magyarázása",
+    partA: [19, 79, 139],
+    partB: [49, 109, 169],
+  },
+  {
+    name: "ismeret",
+    partA: [20, 80, 140],
+    partB: [50, 110, 170],
+  },
+  {
+    name: "vezetés",
+    partA: [21, 81, 141],
+    partB: [51, 111, 171],
+  },
+  {
+    name: "szenvedésre való kül.képesség",
+    partA: [22, 82, 142],
+    partB: [52, 112, 172],
+  },
+  {
+    name: "irgalmasság",
+    partA: [23, 83, 143],
+    partB: [53, 113, 173],
+  },
+  {
+    name: "csodatétel",
+    partA: [24, 84, 144],
+    partB: [54, 114, 174],
+  },
+  {
+    name: "apostolkodás",
+    partA: [25, 85, 145],
+    partB: [55, 115, 175],
+  },
+  {
+    name: "pásztorálás",
+    partA: [26, 86, 146],
+    partB: [56, 116, 176],
+  },
+  {
+    name: "prófétálás",
+    partA: [27, 87, 147],
+    partB: [57, 117, 177],
+  },
+  {
+    name: "szolgálás",
+    partA: [28, 88, 148],
+    partB: [58, 118, 178],
+  },
+  {
+    name: "ének-zene",
+    partA: [29, 89, 149],
+    partB: [59, 119, 179],
+  },
+  {
+    name: "művészet-kreativitás",
+    partA: [30, 90, 150],
+    partB: [60, 120, 180],
+  },
+];
+
+export default gifts;
