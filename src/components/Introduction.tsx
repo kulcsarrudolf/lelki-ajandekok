8df120ad72aab91e5727c65c90ada54d4d30ce9f
Kulcsar Rudolf <kulcsarrudolf@gmail.com>
2023-11-09 14:27:51 +0200

    őőőőőőőőőő

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -40,20 +40,20 @@ const Introduction = ({ goForward }: IntroductionProps) => {
   return (
     <>
       <p>
-        Minden hívõnek teremnie kell a Lélek gyümölcsét. Minden újjászületett
-        hívõ ember kapott legalább egy lelki ajándékot (karizmát), hogy
+        Minden hívőnek teremnie kell a Lélek gyümölcsét. Minden újjászületett
+        hívő ember kapott legalább egy lelki ajándékot (karizmát), hogy
         eszközként használva boldogan, eredményesen, harmónikusan végezze
         szolgálatát Isten gyermekeként a gyülekezetben. Isten szeretetének,
         evangéliumának továbbadására, a sötétség visszaszorítására, sebek
         enyhítésére, Isten országa építésére. E közre adott teszt segítség lehet
-        Istentõl kapott lelkiajándékaid felismerésére, elfogadására,
-        kifejlesztésére, megfelelõ szolgálati terület megtalálásához. Az Úr azt
+        Istentől kapott lelkiajándékaid felismerésére, elfogadására,
+        kifejlesztésére, megfelelő szolgálati terület megtalálásához. Az Úr azt
         akarja, hogy tudjuk, milyen lelki ajándékot kaptunk, s azzal
         sáfárkodjunk örömmel.
       </p>
       <p className="mt-4 font-bold">
         Olvasd el a 180 mondatot, és jelöld meg az öt válasz közül azt, amelyik
-        legközelebb áll hozzád. Adj õszinte és spontán választ!
+        legközelebb áll hozzád. Adj őszinte és spontán választ!
       </p>
       <p className="font-bold italic mt-4">
         Sok örömet kívánunk neked a teszt elkészítése közben!
